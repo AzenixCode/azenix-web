c43dd4474fba5c82e6eff2a43adb4156f75d65e3
dnovacik <novacik.daniel@gmail.com>
2018-01-27 01:26:27 +0100

    resolves #2 Added general SEO and optimized page caching

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var router = express.Router();
 var path = __dirname + "/public/views/";
 
 app.use(compression());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', { maxage: '1d' }));
 
 app.get("/", function (req, res) {
+    res.setHeader("Cache-Control", "public, max-age=86400");
+    res.setHeader("Expires", new Date(Date.now() + 86400).toUTCString());
     res.sendFile(__dirname + "/public/views/index.html");
   });
 
